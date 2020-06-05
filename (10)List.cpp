760692ad6594d14b38cd09a438fd87cf3540b12c
Nurul Ahad Farhan <61065246+farhanahad@users.noreply.github.com>
2020-06-05 19:58:13 +0600

    Add files via upload

---
+++ (10)List.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<list>
+#include<iterator>
+using namespace std;
+int main()
+{
+    list <float> List;
+    list <float>::iterator it;
+    List.push_back(10);
+     List.push_back(19);
+      List.push_front(100);
+       List.push_back(1);
+       for(it=List.begin();it!=List.end();it++)
+        cout<<*it<<endl;
+}
