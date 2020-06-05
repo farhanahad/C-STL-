c619eb0100a093ec9315624b9f4a9cfdabf884a4
Nurul Ahad Farhan <61065246+farhanahad@users.noreply.github.com>
2020-06-06 02:50:16 +0600

    Add files via upload

---
+++ (19)list(empty()-i).cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<list>
+#include<iterator>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int arr[5]={1,2,3,4,5};
+    list <int> mylist(arr,arr+5);
+    if(mylist.empty())
+    {
+        cout<<"Empty";
+    }
+    else
+        cout<<"Filled";
+}
